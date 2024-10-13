be087d6592bfd4b88204bb8b82d549fc1a64de28
Michael Achile <mhykhelofficial@gmail.com>
2024-10-13 09:51:01 +0100

    learn next.js

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,15 @@
+import './ui/global.css'
+import { inter } from './ui/fonts'
+import clsx from 'clsx'
+
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode;
+  children: React.ReactNode
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={clsx(inter.className, 'antialiased')}>{children}</body>
     </html>
-  );
+  )
 }
